c565ad4a109ba4eac369d4b3c52e83ec5337ad5a
APTEM <semen.tombov@yandex.ru>
2021-02-06 19:12:27 +0300

    2 задача

--- main.py
+++ main.py
@@ -3,38 +3,52 @@ import sys
 
 import pygame
 import requests
+api_server = "http://static-maps.yandex.ru/1.x/"
 lon = str(float(input()))
 lat = str(float(input()))
 delta = str(float(input()))
-api_server = "http://static-maps.yandex.ru/1.x/"
 
-params = {
-    "ll": ",".join([lon, lat]),
-    "spn": ",".join([delta, delta]),
-    "l": "map"
-}
-response = requests.get(api_server, params=params)
-# 2.28133742069
-if not response:
-    print("Ошибка выполнения запроса:")
-    print("Http статус:", response.status_code, "(", response.reason, ")")
-    sys.exit(1)
 
-# Запишем полученное изображение в файл.
-map_file = "map.png"
-with open(map_file, "wb") as file:
-    file.write(response.content)
+def delta_lon_lat(delta, lon, lat):
+    params = {
+        "ll": ",".join([lon, lat]),
+        "spn": ",".join([delta, delta]),
+        "l": "map"
+    }
+    response = requests.get(api_server, params=params)
+    if not response:
+        print("Ошибка выполнения запроса:")
+        print("Http статус:", response.status_code, "(", response.reason, ")")
+        sys.exit(1)
 
-# Инициализируем pygame
-pygame.init()
-screen = pygame.display.set_mode((600, 450))
-# Рисуем картинку, загружаемую из только что созданного файла.
-screen.blit(pygame.image.load(map_file), (0, 0))
-# Переключаем экран и ждем закрытия окна.
-pygame.display.flip()
-while pygame.event.wait().type != pygame.QUIT:
-    pass
-pygame.quit()
+    # Запишем полученное изображение в файл.
+    map_file = "map.png"
+    with open(map_file, "wb") as file:
+        file.write(response.content)
 
-# Удаляем за собой файл с изображением.
-os.remove(map_file)
+
+if __name__ == '__main__':
+    clock = pygame.time.Clock()
+    pygame.init()
+    pygame.display.set_caption('Мордобилити')
+    running = True
+    screen = pygame.display.set_mode((600, 450))
+    delta_lon_lat(delta, lon, lat)
+    while running:
+        for event in pygame.event.get():  # обработка нажатий
+            if event.type == pygame.QUIT:
+                running = False
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_PAGEUP:
+                    delta = str(float(delta) * 1.5)
+                if event.key == pygame.K_PAGEDOWN:
+                    delta = str(float(delta) / 1.5)
+                if float(delta) > 90:
+                    delta = '90'
+                print(delta)
+                delta_lon_lat(delta, lon, lat)
+        screen.blit(pygame.image.load("map.png"), (0, 0))
+        pygame.display.flip()
+        clock.tick(30)
+    os.remove("map.png")
+    pygame.quit()
