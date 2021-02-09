9a8823fcd421860d805e672ad4335ea883b0dc1d
APTEM <semen.tombov@yandex.ru>
2021-02-09 21:39:59 +0300

    4 задача

--- main.py
+++ main.py
@@ -9,11 +9,28 @@ lat = str(float(input()))
 delta = str(float(input()))
 
 
-def delta_lon_lat(delta, lon, lat):
+def load_image(name, colorkey=None):
+    fullname = os.path.join('data', name)
+    # если файл не существует, то выходим
+    if not os.path.isfile(fullname):
+        print(f"Файл с изображением '{fullname}' не найден")
+        sys.exit()
+    image = pygame.image.load(fullname)
+    if colorkey is not None:
+        image = image.convert()
+        if colorkey == -1:
+            colorkey = image.get_at((0, 0))
+        image.set_colorkey(colorkey)
+    else:
+        image = image.convert_alpha()
+    return image
+
+
+def delta_lon_lat_layers(delta, lon, lat, layer):
     params = {
         "ll": ",".join([lon, lat]),
         "spn": ",".join([delta, delta]),
-        "l": "map"
+        "l": layer
     }
     response = requests.get(api_server, params=params)
     if not response:
@@ -32,10 +49,33 @@ if __name__ == '__main__':
     pygame.init()
     pygame.display.set_caption('Мордобилити')
     running = True
-    screen = pygame.display.set_mode((600, 450))
-    delta_lon_lat(delta, lon, lat)
+    layer = 'map'
+    screen = pygame.display.set_mode((800, 650))
+    delta_lon_lat_layers(delta, lon, lat, layer)
+    all_sprites = pygame.sprite.Group()
+    # создадим спрайт
+    sprite = pygame.sprite.Sprite()
+    # определим его вид
+    sprite.image = load_image("shema.png")
+    # и размеры
+    sprite.rect = sprite.image.get_rect()
+    sprite.rect.x = 10
+    sprite.rect.y = 460
+    # добавим спрайт в группу
+    all_sprites.add(sprite)
     while running:
         for event in pygame.event.get():  # обработка нажатий
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if 10 < pygame.mouse.get_pos()[0] < 85 and 460 < pygame.mouse.get_pos()[1] < 495 and layer != 'map':
+                    layer = 'map'
+                    sprite.image = load_image("shema.png")
+                elif 87 < pygame.mouse.get_pos()[0] < 162 and 460 < pygame.mouse.get_pos()[1] < 495 and layer != 'sat':
+                    layer = 'sat'
+                    sprite.image = load_image("spytnik.png")
+                elif 164 < pygame.mouse.get_pos()[0] < 240 and 460 < pygame.mouse.get_pos()[1] < 495 and layer != 'skl':
+                    layer = 'sat,skl'
+                    sprite.image = load_image("gibrid.png")
+                delta_lon_lat_layers(delta, lon, lat, layer)
             if event.type == pygame.QUIT:
                 running = False
             if event.type == pygame.KEYDOWN:
@@ -63,10 +103,9 @@ if __name__ == '__main__':
                     lat = str(float(lat) - 170)
                 elif float(lat) < -85:
                     lat = str(float(lat) + 170)
-                print(lat)
-                print(delta)
-                delta_lon_lat(delta, lon, lat)
+                delta_lon_lat_layers(delta, lon, lat, layer)
         screen.blit(pygame.image.load("map.png"), (0, 0))
+        all_sprites.draw(screen)
         pygame.display.flip()
         clock.tick(30)
     os.remove("map.png")
